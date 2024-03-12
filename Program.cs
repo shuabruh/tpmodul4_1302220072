271a563a5728c0837676c780897bf3dd15a1551e
shuabruh <shuabruh@student.telkomuniversity.ac.id>
2024-03-12 15:55:36 +0700

    Implementasi table-driven

--- Program.cs
+++ Program.cs
@@ -1,7 +1,32 @@
-﻿internal class Program
+﻿enum KELURAHAN
+{
+    BATUNANGGAL,
+    KUJANGSARI,
+    MENGGER,
+    WATES,
+    CIJAURA,
+    JATISARI,
+    MARGASARI,
+    SEKEJATI,
+    KEBONWARU,
+    MALEER,
+    SAMOJA,
+};
+
+class KodePos
+{
+    public static int getKodePos(KELURAHAN kelurahan)
+    {
+        int[] kodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+        return kodePos[(int)kelurahan];
+    }
+}
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int kPos = KodePos.getKodePos(KELURAHAN.JATISARI);
+        Console.WriteLine("Kode pos Jatisari adalah: " + kPos);
     }
 }
--- obj/Debug/net8.0/tp-4.AssemblyInfo.cs
+++ obj/Debug/net8.0/tp-4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tp-4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20dc84902f4acf3dab8f98fa893effc9e8492880")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e3c60a343320a41f88d49e8bb383593089f2ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("tp-4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tp-4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
